393146585ca28edeb408a1264338f92629488641
Per Sandberg <per.sandberg@saabgroup.com>
2020-03-06 08:14:11 +0100

    Make it possible to run test standalone

--- regresion/0006_ExecutableProjectWidthAll/test.py
+++ regresion/0006_ExecutableProjectWidthAll/test.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+from os.path import *
+sys.path.append(dirname(dirname(abspath(__file__))))
+
 from test_support import *
-# from
+
+
 run("ssprep",["ssprep.simpleExecutableProject","-Dproject=test0002","-dWITH_ASIS=True","-dWITH_XMLAda=True","-dWITH_AWS=True"])
 exec_cmd("make",["-C", "output/test0002" ,"compile"])
 exec_cmd("output/test0002/bin/test0002-main",["--version"],"output/version")
